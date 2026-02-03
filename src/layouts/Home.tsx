34d150f80f5f5f3660e6d5527da361b04ed60d32
rohit.thapa <rohit.thapa@teksewa.com>
2026-02-03 12:14:08 +0545

    retrying deployment

--- src/layouts/Home.tsx
+++ src/layouts/Home.tsx
@@ -1,24 +1,24 @@
-// import Carousel from "react-multi-carousel";
-// import "react-multi-carousel/lib/styles.css";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
-// import swipper1 from "../../src/assets/images/swipper1.jpg";
-// import swipper2 from "../../src/assets/images/swipper2.jpg";
-// import swipper3 from "../../src/assets/images/swipper3.jpg";
+import swipper1 from "../../src/assets/images/swipper1.jpg";
+import swipper2 from "../../src/assets/images/swipper2.jpg";
+import swipper3 from "../../src/assets/images/swipper3.jpg";
 
-// const CarouselComponent = (Carousel as any).default || Carousel;
+const CarouselComponent = (Carousel as any).default || Carousel;
 
 export default function Home() {
-  // const responsive = {
-  //   xl: { breakpoint: { max: 3000, min: 1280 }, items: 4 },
-  //   lg: { breakpoint: { max: 1280, min: 1024 }, items: 3 },
-  //   md: { breakpoint: { max: 1024, min: 768 }, items: 2 },
-  //   sm: { breakpoint: { max: 768, min: 640 }, items: 2 },
-  //   xs: { breakpoint: { max: 640, min: 0 }, items: 1 },
-  // };
+  const responsive = {
+    xl: { breakpoint: { max: 3000, min: 1280 }, items: 4 },
+    lg: { breakpoint: { max: 1280, min: 1024 }, items: 3 },
+    md: { breakpoint: { max: 1024, min: 768 }, items: 2 },
+    sm: { breakpoint: { max: 768, min: 640 }, items: 2 },
+    xs: { breakpoint: { max: 640, min: 0 }, items: 1 },
+  };
 
   return (
     <div className="w-full min-h-full px-6 py-10 bg-gray-600">
-      {/* <CarouselComponent
+      <CarouselComponent
         responsive={responsive}
         infinite
         autoPlay
@@ -41,8 +41,7 @@ export default function Home() {
 </div>
 
         ))}
-      </CarouselComponent> */}
-      Hello
+      </CarouselComponent>
     </div>
   );
 }
